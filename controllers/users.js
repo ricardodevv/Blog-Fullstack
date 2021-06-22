919685030fbec86d3981a5b0a646d528a961808b
Ricardo Quintero <ricardoaqf.dev@gmail.com>
2021-06-21 23:18:00 -0500

    push heroku

--- controllers/users.js
+++ controllers/users.js
@@ -1,25 +1,5 @@
-const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/users')
-
-/*
-  usersRouter.post('/', async (request, response) => {
-  const body = request.body
-
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(body.password, saltRounds)
-
-  const user = new User({
-    username: body.username,
-    name: body.name,
-    passwordHash,
-  })
-
-  const savedUser = await user.save()
-
-  response.json(savedUser)
-})
-*/
 
 usersRouter.get('/', async (request, response) => {
   const users = await User
